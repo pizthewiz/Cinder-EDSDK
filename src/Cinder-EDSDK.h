ed8d5589f30c96a581dc633b30a38564754621ed
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2013-12-11 11:16:32 -0800

    add block umbrella header

---
+++ src/Cinder-EDSDK.h
@@ -0,0 +1,12 @@
+//
+//  Cinder-EDSDK.h
+//  Cinder-EDSDK
+//
+//  Created by Jean-Pierre Mouilleseaux on 10 Dec 2013.
+//  Copyright 2013 Chorded Constructions. All rights reserved.
+//
+
+#pragma once
+
+#include "Camera.h"
+#include "CameraBrowser.h"
